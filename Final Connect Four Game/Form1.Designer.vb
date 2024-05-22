1fd93a7d69f688017b49d87385b8acd949268b63
KamiilSW <kam.swierkosz@gmail.com>
2024-05-22 14:14:07 +0100

    Add project files.

---
+++ Final Connect Four Game/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Panel1 = New Panel()
+        TextBox1 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.Location = New Point(12, 12)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(572, 687)
+        Panel1.TabIndex = 0
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BackColor = Color.White
+        TextBox1.ForeColor = Color.Red
+        TextBox1.Location = New Point(603, 299)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(58, 27)
+        TextBox1.TabIndex = 1
+        TextBox1.Text = "Red"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(694, 711)
+        Controls.Add(TextBox1)
+        Controls.Add(Panel1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents TextBox1 As TextBox
+
+End Class
---
+++ Final Connect Four Game/Form1.vb
@@ -0,0 +1,44 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim gridSize As Integer = 7 'Amount of rows and colums!
+        Dim buttonSize As Integer = 70 ' Size of buttons!
+
+        For i As Integer = 0 To gridSize - 1
+
+            For j As Integer = 0 To gridSize - 1
+                Dim button As New Button()
+                button.Size = New Size(buttonSize, buttonSize)
+                button.Location = New Point(j * buttonSize, i * buttonSize)
+                button.Text = ""
+                AddHandler button.Click, AddressOf Button_Click
+                Panel1.Controls.Add(button)
+
+            Next
+
+        Next
+    End Sub
+    Private Sub Button_Click(sender As Object, e As EventArgs)
+        Dim clickedButton As Button = CType(sender, Button)
+
+        If clickedButton Then
+            If TextBox1.Text = "Red" Then
+            clickedButton.BackColor = Color.Red
+
+        Else
+            clickedButton.BackColor = Color.Blue
+
+        End If
+
+        If TextBox1.Text = "Red" Then
+            TextBox1.Text = "Blue"
+            TextBox1.ForeColor = Color.Blue
+
+        Else
+            TextBox1.Text = "Red"
+            TextBox1.ForeColor = Color.Red
+
+        End If
+    End Sub
+
+End Class
